7a68277a361a00cda47ef8b9624925ba116aec9a
Alex Wehn <alex@wehn.io>
2016-03-02 20:53:16 -0600

    Added configuration and the ability to publish messages to SNS

--- httpEventLambda.js
+++ httpEventLambda.js
@@ -1,8 +1,24 @@
 'use strict';
 
+var AWS = require("aws-sdk");
+var config = require("./config");
+
+AWS.config = {
+    region: config.region
+};
+
+
 exports.handler = function(event, context) {
-  console.log("Event type: %s",event.eventType);
+  console.log("Event topic: %s",event.eventTopic);
   console.log("Event subject: %s",event.eventSubject);
   console.log("Event body: %s",event.eventBody);
-  context.done(null,'Success!');
+
+  var arn = "arn:"+config.partition+":sns:"+config.region+":"+config.accountId+":"+event.eventTopic;
+  var sns = new AWS.SNS();
+  var params = {
+          Message: event.eventBody,
+          Subject: event.eventSubject,
+          TopicArn: arn
+      };
+      sns.publish(params, context.done);
 };
